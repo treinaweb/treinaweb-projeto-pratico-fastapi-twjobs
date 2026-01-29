dc37eb5917b824fa2cac62ebcdc655c3baaf8381
Cleyson Lima <cleysonph@gmail.com>
2026-01-29 18:41:02 -0300

    02:11 - Utilizando migrações no banco de dados com o Alembic

--- twjobs/core/models.py
+++ twjobs/core/models.py
@@ -1,7 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_as_dataclass, mapped_column, registry
 
-from .db import engine
-
 table_registry = registry()
 
 
@@ -13,6 +11,3 @@ class Skill:
         init=False, primary_key=True, autoincrement=True
     )
     name: Mapped[str] = mapped_column(unique=True)
-
-
-table_registry.metadata.create_all(engine)
